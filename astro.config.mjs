1f5b49f72bf4184aedf7985b7721e510c08be350
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-03-18 19:34:42 +0100

    Set up Astro with React and Tailwind CSS; add TypeScript configuration and update .gitignore

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+})
