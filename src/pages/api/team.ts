51c684a9dd0e19594c9bf17fb0b140a96aec911a
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-03-19 16:43:45 +0100

    Add new API endpoint for team data retrieval; implement GET method to handle query parameters and return appropriate responses based on input validation and fetch results.

---
+++ src/pages/api/team.ts
@@ -0,0 +1,32 @@
+import { API_URL } from 'astro:env/server';
+
+export async function GET({ request }) {
+  const { searchParams } = new URL(request.url);
+
+  const group = searchParams.get('group');
+  const gender = searchParams.get('gender');
+  const name = searchParams.get('name');
+
+  if (!group || !gender || !name) {
+    return new Response(JSON.stringify({ message: 'Group, gender and name are required' }), { status: 400 });
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/team?${searchParams.toString()}`);
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        ...response.headers,
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    const { status, message } = error instanceof Error
+      ? { status: ('status' in error ? error.status : 500) as number, message: error.message }
+      : { status: 500, message: 'Internal server error' };
+    return new Response(JSON.stringify({ message }), { status });
+  }
+}
