c073b7483c613b5a4877d738a9a72d0fe9e9f665
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-03-26 14:18:57 +0100

    refactor: Update footer component to include dynamic app version, author name, and repository link; add LinkedIn URL constant for improved footer information.

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -4,3 +4,5 @@ export const APP_VERSION = pkg.version;
 export const APP_NAME = pkg.name;
 export const AUTHOR_NAME = pkg.author;
 export const BASE_IMAGE_URL = '/favicon.png';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/maximilianogarcia13';
+export const REPOSITORY_URL = pkg.repository.url.replace('git+', '');
